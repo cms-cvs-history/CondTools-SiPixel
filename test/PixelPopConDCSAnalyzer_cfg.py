e85027e7f008f1d2d70064c0477ac36dd0dcf140
cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch>
2008-12-11 03:12:06 +0000

    Move src/PixelPopConDCSSourceHandler.cc to plugins/PixelPopConDCSAnalyzer.cc, remove plugin flag from BuildFile, create cfi for plugins/PixelPopConDCSAnalyzer.

--- test/PixelPopConDCSAnalyzer_cfg.py
+++ test/PixelPopConDCSAnalyzer_cfg.py
@@ -1,6 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("PixelPopConTest")
+process = cms.Process("PixelPopConDCSTest")
+
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+process.load("CondTools.SiPixel.PixelPopConDCSAnalyzer_cfi")
 
 process.source = cms.Source("EmptyIOVSource",
 
@@ -9,9 +12,6 @@ process.source = cms.Source("EmptyIOVSource",
   interval = cms.uint64(1),
   timetype = cms.string('runnumber')
 )
-
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
-#process.CondDBSetup.DBParameters.authenticationPath = '/afs/cern.ch/cms/DB/conddb'
 
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
 
@@ -26,48 +26,6 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     cms.PSet( record = cms.string('PixelCaenChannelIsOnRcd'), tag = cms.string('IsOn') ),
     cms.PSet( record = cms.string('PixelCaenChannelIMonRcd'), tag = cms.string('IMon') ),
     cms.PSet( record = cms.string('PixelCaenChannelRcd'), tag = cms.string('All') )
-  )
-)
-
-process.PixelCaenChannelIsOn = cms.EDAnalyzer("PixelPopConBoolAnalyzer",
-
-  record = cms.string('PixelCaenChannelIsOnRcd'),
-  loggingOn = cms.untracked.bool(True),
-  SinceAppendMode = cms.bool(True),
-
-  Source = cms.PSet
-  (
-    authenticationPath = cms.string('.'),
-    dbName = cms.string('CMSONR'),
-    tables = cms.vstring('DCSLASTVALUE_ON')
-  )
-)
-
-process.PixelCaenChannelIMon = cms.EDAnalyzer("PixelPopConFloatAnalyzer",
-
-  record = cms.string('PixelCaenChannelIMonRcd'),
-  loggingOn = cms.untracked.bool(True),
-  SinceAppendMode = cms.bool(True),
-
-  Source = cms.PSet
-  (
-    authenticationPath = cms.string('.'),
-    dbName = cms.string('CMSONR'),
-    tables = cms.vstring('DCSLASTVALUE_CURRENT')
-  )
-)
-
-process.PixelCaenChannel = cms.EDAnalyzer("PixelPopConCaenChannelAnalyzer",
-
-  record = cms.string('PixelCaenChannelRcd'),
-  loggingOn = cms.untracked.bool(True),
-  SinceAppendMode = cms.bool(True),
-
-  Source = cms.PSet
-  (
-    authenticationPath = cms.string('.'),
-    dbName = cms.string('CMSONR'),
-    tables = cms.vstring('DCSLASTVALUE_ON', 'DCSLASTVALUE_CURRENT', 'DCSLASTVALUE_VOLTAGE')
   )
 )
 
